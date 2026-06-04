8935e81f6926b9e299de3dd531c6343f9e75bcdf
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-06-04 12:52:53 +0530

    Create --Given a table of Facebook posts, for each user who posted at least twice in 2021.sql

---
+++ daily_practice/day-15/--Given a table of Facebook posts, for each user who posted at least twice in 2021.sql
@@ -0,0 +1,11 @@
+---Given a table of Facebook posts, for each user who posted at least twice in 2021, write a query to find the number of days between each user’s 
+--first post of the year and last post of the year in the year 2021.
+----Output the user and number of the days between each user's first and last post
+
+
+SELECT user_id,
+      datediff(max(post_date),min(post_date)) as daysbetween
+      from posts
+      where year(post_date) = 2021
+      group by user_id
+      having count(post_id) >=2
