ec54d2aafca33726794f17742ac95c88a64350aa
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-06-11 18:15:18 +0530

    Create Find countries where the Top Quartile (Top 25%) customers contribute more than 70% of country sales.  with cte1 as.sql

---
+++ daily_practice/day-20/Find countries where the Top Quartile (Top 25%) customers contribute more than 70% of country sales.  with cte1 as.sql
@@ -0,0 +1,30 @@
+---Find countries where the Top Quartile (Top 25%) customers contribute more than 70% of country sales.
+
+with cte1 as
+(
+select dc.country,
+       fs.customer_key,
+       sum(fs.sales_amount) as totalsales,
+       PERCENT_RANK()over(partition by dc.country order by sum(fs.sales_amount)desc) as pctrank
+       from gold.fact_sales as fs
+       left join gold.dim_customers as dc
+       on fs.customer_key = dc.customer_key
+       group by fs.customer_key,dc.country)
+       , cte2 as 
+       ( select country,
+                sum(totalsales) as top25sales
+                from cte1
+                where pctrank <=0.25
+                group by country)
+                , cte3 as 
+                (select country,
+                        sum(totalsales) as countrysales
+                        from cte1
+                        group by country)
+
+                        select *,
+                               round(100.0* top25sales/countrysales,2) as pctsales
+                               from cte2 as c2
+                               join cte3 as c3
+                               on c2.country = c3.country
+                               where top25sales > 0.70*countrysales
