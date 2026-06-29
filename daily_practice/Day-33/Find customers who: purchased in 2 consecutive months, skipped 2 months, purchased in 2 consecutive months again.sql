035d445bfa79fa91a2b8da5ebc2a6deb0cbba277
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-06-29 20:33:50 +0530

    Create Find customers who: purchased in 2 consecutive months, skipped 2 months, purchased in 2 consecutive months again.sql

---
+++ daily_practice/Day-33/Find customers who: purchased in 2 consecutive months, skipped 2 months, purchased in 2 consecutive months again.sql
@@ -0,0 +1,30 @@
+----Find customers who: purchased in 2 consecutive months, skipped 2 months, purchased in 2 consecutive months again
+
+
+with cte1 as
+(
+select distinct
+       dc.customer_id,
+       concat(dc.first_name,' ', dc.last_name) as customer_name,
+       DATEFROMPARTS(year(order_date),month(order_date),1) as purchase_month
+      from gold.fact_sales as fs
+      left join gold.dim_customers as dc
+      on fs.customer_key = dc.customer_key
+      )
+      , months as
+      (select *,
+              lead(purchase_month,1)over(partition by customer_id order by purchase_month) as lead1,
+              lead(purchase_month,2)over(partition by customer_id order by purchase_month) as lead2,
+              lead(purchase_month,3)over(partition by customer_id order by purchase_month) as lead3
+              from cte1
+              )
+              select customer_id,
+                     customer_name,
+                     purchase_month,
+                     lead1 as second_purchase_month,
+                     lead2 as third_purchase_month,
+                     lead3 as fourth_purchase_month
+                     from months
+                     where datediff(month, purchase_month, lead1) = 1
+                     and datediff(month, lead1, lead2) = 3
+                     and datediff(month, lead2, lead3) = 1
