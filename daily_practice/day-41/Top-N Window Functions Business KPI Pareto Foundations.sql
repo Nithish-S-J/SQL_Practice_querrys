95ed6c6c813d7e83d8c46bcf6ad5fe822f421c3c
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-07-13 20:54:13 +0530

    Create Top-N Window Functions Business KPI Pareto Foundations.sql

---
+++ daily_practice/day-41/Top-N Window Functions Business KPI Pareto Foundations.sql
@@ -0,0 +1,41 @@
+/* For every country, 
+calculate
+Top 5 customer revenue
+Country revenue
+Contribution % 
+Return ; country , top5_customer_revenue, country_revenue, top5_contribution_pct */
+
+with cte1 as
+(
+select 
+       dc.country,
+       dc.customer_id,
+       sum(sales_amount) as total_sales,
+       rank()over(partition by country order by sum(sales_amount)desc) as rn
+       from gold.fact_sales as fs
+       left join gold.dim_customers as dc
+       on fs.customer_key = dc.customer_key
+       group by country, dc.customer_id
+       )
+       ,cte2 as
+       (select country,
+               sum(total_sales) as top5_customer_revenue
+               from cte1
+               where rn <= 5
+               group by country
+               )
+               ,cte3 as
+               (select country,
+                       sum(total_sales) as country_revenue
+                       from cte1
+                       group by country
+                       )
+
+                       select 
+                             c2.country,
+                             c2.top5_customer_revenue,
+                             c3.country_revenue,
+                             round(100.0*top5_customer_revenue/country_revenue,2) as contribution_pct
+                             from cte2 as c2
+                             join cte3 as c3
+                             on c2.country = c3.country
