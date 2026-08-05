d4de64232f19067a200b956394d2dad02ec4ea31
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-08-05 20:20:39 +0530

    Create Product team wants to identify products that have never generated a sale.sql

---
+++ daily_practice/day-54/Product team wants to identify products that have never generated a sale.sql
@@ -0,0 +1,19 @@
+/* The Product team wants to identify products that have never generated a sale.
+
+Return
+product_key
+product_name
+category
+subcategory
+start_date */
+
+select 
+       dp.product_key,
+       dp.product_name,
+       dp.category,
+       dp.subcategory,
+       dp.start_date
+       from gold.dim_products as dp
+       left join gold.fact_sales as fs
+       on dp.product_key = fs.product_key
+       where fs.product_key is null
