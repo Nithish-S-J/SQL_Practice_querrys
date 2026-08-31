b16bd9a928d22ec8a93358bcb15f0bf029499484
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-08-31 23:36:52 +0530

    Update For every customer, find their first-ever calendar month of purchase and the revenue they generated in that month.sql

--- daily_practice/day-72/For every customer, find their first-ever calendar month of purchase and the revenue they generated in that month.sql
+++ daily_practice/day-72/For every customer, find their first-ever calendar month of purchase and the revenue they generated in that month.sql
@@ -1,1 +1,35 @@
-.
+WITH cte1 AS
+(
+    SELECT
+        dc.customer_id,
+        dc.first_name AS customer_name,
+        dc.country,
+        DATEFROMPARTS(YEAR(order_date), MONTH(order_date), 1) AS first_purchase_month,
+        SUM(sales_amount) AS first_month_revenue
+    FROM gold.fact_sales AS fs
+    LEFT JOIN gold.dim_customers AS dc
+        ON fs.customer_key = dc.customer_key
+    GROUP BY
+        dc.customer_id,
+        dc.first_name,
+        dc.country,
+        DATEFROMPARTS(YEAR(order_date), MONTH(order_date), 1)
+),
+cte2 AS
+(
+    SELECT
+        *,
+        ROW_NUMBER() OVER(
+            PARTITION BY customer_id
+            ORDER BY first_purchase_month
+        ) AS rn
+    FROM cte1
+)
+SELECT
+    customer_id,
+    customer_name,
+    country,
+    first_purchase_month,
+    first_month_revenue
+FROM cte2
+WHERE rn = 1;
